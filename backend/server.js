21abdaa3549f0e2560ccd521623da18fa311cd91
Amy Arora <amyarora2214@gmail.cpm>
2024-07-18 19:02:59 +0530

    1- post routes and ccontrollers added

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,9 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { v2 as cloudinary } from "cloudinary";
 
-
 import authRoutes from "./routes/auth.routes.js";
-import userRoutes from "./routes/user.routes.js";
-
+import userRoutes from "./routes/user.routes.js"; 
+import postRoutes from "./routes/post.routes.js"; 
 
 import connectMongoDB from "./db/connectMongoDB.js";
 
@@ -29,6 +28,7 @@ app.use(cookieParser());
 
 app.use("/api/auth",authRoutes);
 app.use("/api/users",userRoutes);
+app.use("/api/posts",postRoutes);
 
 app.listen(PORT, () => {
     console.log('Server is running on port ${PORT}');
