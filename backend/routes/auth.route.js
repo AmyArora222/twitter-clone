1fe8a3a4c2ab1821b03f9f4c993a6239b9f25671
Amy Arora <amyarora2214@gmail.cpm>
2024-07-19 16:12:36 +0530

    3- Auth handled and routes protected in client

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,6 +1,6 @@
 import express from"express";
 import { getMe, login,logout,signup } from '../controllers/auth.controller.js';
-import { protectRoute } from "../middleware/protectRoute.js";
+import { protectRoute } from "../middleware/protectRoute.js"; 
 
 const router = express.Router();
 
--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,6 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());  //to parse req.body
 app.use(express.urlencoded({extended: true})); //to parse form data(urlencoded)
 
-
 app.use(cookieParser());
 
 app.use("/api/auth",authRoutes);
