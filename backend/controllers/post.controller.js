c8066b31502420dce8a81cdd13a95aaeac2ce9d0
Amy Arora <amyarora2214@gmail.cpm>
2024-07-20 02:24:52 +0530

    4-prepared our app for deployment

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -105,8 +105,8 @@ export const likeUnlikePost = async (req, res) => {
 			await Post.updateOne({ _id: postId }, { $pull: { likes: userId } });
 			await User.updateOne({ _id: userId }, { $pull: { likedPosts: postId } });
 
-			// const updatedLikes = post.likes.filter((id) => id.toString() !== userId.toString());
-			res.status(200).json({message: "Post unliked successfully"});
+			const updatedLikes = post.likes.filter((id) => id.toString() !== userId.toString());
+			res.status(200).json(updatedLikes);
 		} else {
 			// Like post
 			post.likes.push(userId);
@@ -120,8 +120,8 @@ export const likeUnlikePost = async (req, res) => {
 			});
 			await notification.save();
 
-			// const updatedLikes = post.likes;
-			res.status(200).json({message: "Post liked successfully"});
+			const updatedLikes = post.likes;
+			res.status(200).json(updatedLikes);
 		}
 	} catch (error) {
 		console.log("Error in likeUnlikePost controller: ", error);
